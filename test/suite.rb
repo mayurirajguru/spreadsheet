91b71bca7164209d7cbfb9694d86811db24cdc95
Mayuri RajguruDua <mayurirajguru@gmail.com>
2012-01-09 12:50:19 +0100

    changes to suite.rb for running in all versions

--- test/suite.rb
+++ test/suite.rb
@@ -1,19 +1,21 @@
 #!/usr/bin/env ruby
 # suite.rb -- spreadsheet -- 26.07.2011 -- zdavatz@ywesee.com
+# suite.rb -- spreadsheet -- 09.01.2012 -- mayurirajguru@gmail.com
 
 require 'find'
 require 'rubygems'
 
-here = File.expand_path('./lib', File.dirname(__FILE__))
+#here = File.expand_path('./lib', File.dirname(__FILE__))
+here = File.dirname(__FILE__)
 
 $: << here
 
 Find.find(here) do |file|
-	#if /(?<!suite)\.rb$/o.match(file)
-	if /[^suite\W]\.rb$/o.match(file)
-    #from Roel van der Hoorn vanderhoorn@gmail.com
-    #should work for Ruby 1.8 and 1.9, without Oniguruma
-#  if /(?:^|\/)(?!suite)[^\/]+\.rb$/o
+	if /[^suite\W]\.rb$/o.match(file)    
+		file = "./" + file #fix
+		# i'd rather use require_relative instead of appending ./ in file - Mayuri 
+		# but require relative won't work in 1.8.6 ruby and is available for 1.9.x 
+		#this fix works in all the versions 
     require file
 	end
 end
